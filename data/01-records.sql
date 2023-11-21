47f2243988fd8c16c42d18838d4ccd46fbf0cc6a
jeffersonamado <jeffersonamado@gmail.com>
2023-11-21 11:11:07 -0500

    feat(populate): add records to db tables

---
+++ data/01-records.sql
@@ -0,0 +1,10 @@
+INSERT INTO curso(nombre, creditos)
+VALUES
+	('750006C-BASES DE DATOS', '4'),
+	('750009C-SISTEMAS OPERATIVOS', '3');
+
+INSERT INTO estudiante(nombre, apellidos)
+VALUES
+	('Jefferson Amado', 'Peña'),
+	('Messi Alejandro', 'Ramirez'),
+	('Diomedes Ricardo', 'Upegui');
---
+++ data/02-triggers.sql
@@ -0,0 +1,4 @@
+-- Cree un disparador que impida que un estudiante
+-- se inscriba a mas cursos 
+-- si ya tiene inscrito  20 creditos
+
